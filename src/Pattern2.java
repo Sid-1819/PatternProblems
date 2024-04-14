3c00d6c39a94e6c6e96c8aaea54f14db358a3d29
Sid-1819 <siddhesh.shirdhankar18@gmail.com>
2024-04-14 12:52:52 +0530

    pattern2

---
+++ src/Pattern2.java
@@ -0,0 +1,21 @@
+public class Pattern2 {
+    /*
+     *
+     **
+     ***
+     ****
+     *****
+     */
+    public static void main(String[] args) {
+pattern(5);
+    }
+    static void pattern(int n ){
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
