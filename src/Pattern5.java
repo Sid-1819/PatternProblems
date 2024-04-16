2edd73330145a5f0cb48523911fe43562f5def39
Sid-1819 <siddhesh.shirdhankar18@gmail.com>
2024-04-16 16:31:08 +0530

    pattern5

--- src/Pattern5.java
+++ src/Pattern5.java
@@ -1,8 +1,26 @@
 public class Pattern5 {
+    /*
+     *
+     **
+     ***
+     ****
+     *****
+     ****
+     ***
+     **
+     *
+     */
     public static void main(String[] args) {
-
+    pattern(5);
     }
     static void pattern(int n){
+        for (int i = 0; i < 2*n; i++) {
+            int totalCol = i>n?2*n-i:i;
+            for (int j = 0; j < totalCol; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
     }
 }
